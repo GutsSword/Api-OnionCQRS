ac5d575e5a7e3e964f1359e98d7b149ff24c72f7
Emirhan Akca <emirhanakkca@gmail.com>
2024-02-24 21:25:28 +0300

    Migrations - DataSeed. DbContext oluşturuldu.

---
+++ Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Hepsi.Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {   
+        }
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Brand> Brands { get; set; }    
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Detail> Details { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
