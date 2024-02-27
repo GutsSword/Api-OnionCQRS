316d22ca8e7cbaf26f78ade6f35c1e7258790468
Emirhan Akca <emirhanakkca@gmail.com>
2024-02-27 21:16:36 +0300

    Features yapısı,Controller eklend.

--- Infrastructure/Persistence/Repositories/WriteRepository.cs
+++ Infrastructure/Persistence/Repositories/WriteRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Hepsi.Api.Application.Interfaces.Repositories;
 using Hepsi.Api.Domain.Common;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistence.Repositories
 {
