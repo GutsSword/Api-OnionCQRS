238a22d45d53cc209acf19150c0a07eae66ce32c
Emirhan Akca <emirhanakkca@gmail.com>
2024-03-12 22:55:36 +0300

    AutoMapper Customized.

--- Core/Hepsi.Api.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
+++ Core/Hepsi.Api.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -1,6 +1,9 @@
-﻿using Hepsi.Api.Application.Interfaces.UnitOfWorks;
+﻿using Hepsi.Api.Application.Dtos;
+using Hepsi.Api.Application.Interfaces.AutoMapper;
+using Hepsi.Api.Application.Interfaces.UnitOfWorks;
 using Hepsi.Api.Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,28 +15,25 @@ namespace Hepsi.Api.Application.Features.Products.Queries.GetAllProducts
     public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQueryRequest, IList<GetAllProductsQueryResponse>>
     {
         private readonly IUnitOfWork unitOfWork;
-        public GetAllProductsQueryHandler(IUnitOfWork unitOfWork)
+        private readonly IMapper mapper;
+
+        public GetAllProductsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
         {
             this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
         }
 
         public async Task<IList<GetAllProductsQueryResponse>> Handle(GetAllProductsQueryRequest request, CancellationToken cancellationToken)
         {
-            var products = await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+            var products = await unitOfWork.GetReadRepository<Product>().GetAllAsync(include: x=>x.Include(x=>x.Brand));
+            var brand = mapper.Map<BrandDto,Brand>(new Brand());
 
-            List<GetAllProductsQueryResponse> response = new();
+            var map = mapper.Map<GetAllProductsQueryResponse, Product>(products);
 
-            foreach(var product in products)
-            {
-                response.Add(new GetAllProductsQueryResponse
-                {
-                    Title = product.Title,
-                    Description = product.Description,
-                    Discount = product.Discount,
-                    Price = product.Price - (product.Price * product.Discount/100),
-                });
-            }
-            return response;
+            foreach (var item in map)
+                item.Price -= (item.Price * item.Discount / 100);
+
+            return map;
         }
     }
 }
