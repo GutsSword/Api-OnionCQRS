fa96033ef1e19f948b04c7415692b4055fbe51da
Emirhan Akca <emirhanakkca@gmail.com>
2024-03-13 18:28:15 +0300

    Global Exception Handling yapıldı.

---
+++ Core/Hepsi.Api.Application/Exceptions/ConfigureExceptionMiddleWare.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hepsi.Api.Application.Exceptions
+{
+    public static class ConfigureExceptionMiddleWare
+    {
+        public static void ConfigureExceptionHandlingMiddleWare(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleWare>();
+        }
+    }
+}
--- Core/Hepsi.Api.Application/Registration.cs
+++ Core/Hepsi.Api.Application/Registration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Hepsi.Api.Application.Exceptions;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace Hepsi.Api.Application
         public static void AddApplication(this IServiceCollection services)
         {
             var assembly = Assembly.GetExecutingAssembly();
+            services.AddTransient<ExceptionMiddleWare>();
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
         }
     }
