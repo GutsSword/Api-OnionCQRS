b477a1e612730079a032be3b02ab3d242d121acb
Emirhan Akca <emirhanakkca@gmail.com>
2024-03-13 21:27:27 +0300

    BaseRule sınıfı oluşturuldu.

--- Core/Hepsi.Api.Application/Features/Products/Command/CreateProduct/CreateProductCommandHandler.cs
+++ Core/Hepsi.Api.Application/Features/Products/Command/CreateProduct/CreateProductCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Hepsi.Api.Application.Interfaces.UnitOfWorks;
+﻿using Hepsi.Api.Application.Features.Products.Rules;
+using Hepsi.Api.Application.Interfaces.UnitOfWorks;
 using Hepsi.Api.Domain.Entities;
 using MediatR;
 using System;
@@ -12,14 +13,21 @@ namespace Hepsi.Api.Application.Features.Products.Command.CreateProduct
     public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest,Unit>
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly ProductRules productRules;
 
-        public CreateProductCommandHandler(IUnitOfWork unitOfWork)
+        public CreateProductCommandHandler(IUnitOfWork unitOfWork,ProductRules productRules)
         {
             this.unitOfWork = unitOfWork;
+            this.productRules = productRules;
         }
         public async Task<Unit> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
         {
+            IList<Product> products = await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+
+            await productRules.ProductTitleMustNotBeSame(products, request.Title);
+
             Product product = new(request.Title, request.Description, request.BrandId, request.Price, request.Discount);
+
             await unitOfWork.GetWriteReporsitory<Product>().AddAsync(product);
             if(await unitOfWork.SaveAsync() is not 0)
             {
